fadce888abf4086eeb1aaac848c829fa7b84ec2f
Cassandra1245 <guillermogarciasuarez@gmail.com>
2026-05-01 16:17:59 +0000

    añadido esqueleto Spring Boot

---
+++ src/main/java/comciberGSG/calculatorGSG/CalculatorGsgApplication.java
@@ -0,0 +1,13 @@
+package comciberGSG.calculatorGSG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGsgApplication.class, args);
+	}
+
+}
